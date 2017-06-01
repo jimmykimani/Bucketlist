d2723e9ee4b0681bc9389791d8cf935cdc4690ba
jimmykimani <james.kimani@andela.com>
2017-06-01 13:56:18 +0300

    (Feature) Add the necessary marshal fields

--- app/bucketlist_api.py
+++ app/bucketlist_api.py
@@ -24,3 +24,21 @@ def verify_token(token):
 # By default, all fields in out return iterable will be rendered as-is.
 # Flask-RESTful provides the fields module and the marshal to help us solve this
 # while working with objects
+
+bucketlist_item_field = {'item_id': fields.Integer,
+                         'item_name': fields.String,
+                         'date_created': fields.DateTime,
+                         'date_modified': fields.DateTime(dt_format='rfc822'),
+                         'done': fields.String
+                         }
+
+
+# Field Marshal for Bucketlist
+bucketlist_field = {'id': fields.Integer,
+                    'name': fields.String,
+                    'date_created': fields.DateTime,
+                    'date_modified': fields.DateTime,
+                    'items': fields.Nested(bucketlist_item_field),
+                    'created_by': fields.String,
+                    'uri': fields.Url('bucket_list.bucketlist'),
+                    }
