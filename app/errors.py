c0255f44c798baf3b7f30e015ce9f5b827074f29
jimmykimani <james.kimani@andela.com>
2017-06-01 14:00:12 +0300

    (Chore) Add custom validation errors and status codes

---
+++ app/errors.py
@@ -0,0 +1,40 @@
+from flask import jsonify
+
+
+class ValidationError(ValueError):
+    """Custom Exception"""
+    pass
+
+
+def forbidden(message):
+    response = jsonify({'status': 403, 'error': 'forbidden',
+                        'message': message})
+    response.status_code = 403
+    return response
+
+
+def not_found(message):
+    response = jsonify({'status': 404, 'error': 'not found',
+                        'message': message})
+    response.status_code = 404
+    return response
+
+
+def bad_request(message):
+    response = jsonify({'status': 400, 'error': 'bad request',
+                        'message': message})
+    response.status_code = 400
+    return response
+
+
+def unauthorized(message):
+    response = jsonify({'status': 401, 'error': 'unauthorized',
+                        'message': message})
+    response.status_code = 401
+    return response
+
+def Conflict(message):
+    response = jsonify({'status': 409, 'error': 'conflict',
+                        'message': message})
+    response.status_code = 409
+    return response
