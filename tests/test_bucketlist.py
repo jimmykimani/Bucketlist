dd0e4bb620d9ee3e7c37c07ea29d6a7eaf3478cd
jimmykimani <james.kimani@andela.com>
2017-06-01 23:38:48 +0300

    (Tests) Update tests to create update and delete bucketlist items

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -7,6 +7,7 @@ from app.models import Bucketlist, Item
 
 
 url = '/api/v1/bucketlists/'
+item_url = '/api/v1/bucketlists/1/items/'
 
 
 class BucketlistTestCase(BaseTestCase):
@@ -95,5 +96,78 @@ class BucketlistTestCase(BaseTestCase):
         # delete bucketlist
         response = self.client.delete(url + '10/',
                                       headers=self.set_header())
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 404)
+
+    def test_create_a_bucketlist_item(self):
+        """ Tests endpoint can create new item."""
+        self.client.post(url,
+                         data=json.dumps(dict(name='new bucketlist')),
+                         headers=self.set_header())
+        response = self.client.post(item_url, data=json.dumps(dict(name='new item')),
+                                    headers=self.set_header())
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 201)
+
+    def test_add_item_with_invalid_bucketlist_id(self):
+        """ Test item endpoint rejects invalid bucketlist id """
+        self.client.post(url,
+                         data=json.dumps(dict(name='new bucketlist')),
+                         headers=self.set_header())
+        response = self.client.post('/api/v1/bucketlists/99/items/',
+                                    data=json.dumps(dict(name='new item')),
+                                    headers=self.set_header())
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 404)
+
+    # def test_get_all_items(self):
+    #     """ Test item endpoint gets items succesfully"""
+    #     # create new item
+    #     self.client.post(url,
+    #                      data=json.dumps(dict(name='new bucketlist')),
+    #                      headers=self.set_header())
+    #     self.client.post(item_url, data=json.dumps(dict(name='new item')),
+    #                                 headers=self.set_header())
+    #     # get all items
+    #     response = self.client.get(item_url, headers=self.set_header())
+    #     self.assertEqual(response.status_code, 200)
+
+    def test_update_items(self):
+        """ Test endpoint updated items succesfully"""
+        # create new item
+        self.client.post(url,
+                         data=json.dumps(dict(name='new bucketlist')),
+                         headers=self.set_header())
+        self.client.post(item_url, data=json.dumps(dict(name='new item')),
+                                    headers=self.set_header())
+        response = self.client.put(item_url+ '1',
+                                   data=json.dumps(
+                                       dict(name='update new item')),
+                                   headers=self.set_header())
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_item(self):
+        """ Test deletion of items """
+        self.client.post(url,
+                         data=json.dumps(dict(name='new bucketlist')),
+                         headers=self.set_header())
+        self.client.post(item_url, data=json.dumps(dict(name='new item')),
+                                    headers=self.set_header())
+        response = self.client.delete(item_url+ '1',
+                                   headers=self.set_header())
+        data = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+
+    def test_delete_with_invalid_item_id(self):
+        """ Test that endpoint rejects deletion with invalid bucketlist_id """
+        # create new item
+        self.client.post(url,
+                         data=json.dumps(dict(name='new bucketlist')),
+                         headers=self.set_header())
+        self.client.post(item_url, data=json.dumps(dict(name='new item')),
+                                    headers=self.set_header())
+        response = self.client.delete(item_url+ '999',
+                                   headers=self.set_header())
         data = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 404)
