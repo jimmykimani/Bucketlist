5d48cff32ff1837e26849063ea08dddbca3af93c
jimmykimani <james.kimani@andela.com>
2017-06-07 22:50:57 +0300

    (Fix) Travis errors

--- tests/base.py
+++ tests/base.py
@@ -44,12 +44,12 @@ class BaseTestCase(unittest.TestCase):
         # A Token is needed to restrict access to certain resources
         # If not included it will result in a 401: Unauthorized Access error.
 
-        self.auth_token = data['auth_token']
+        auth_token = data['auth_token']
 
         # # Helps json to accept a JSON encoded entity from the request body.
         # # Token prefix comes before the token
 
-        return{'Authorization': 'Token ' + self.auth_token,
+        return{'Authorization': 'Token ' + auth_token,
                'Content-Type': 'application/json',
                'Accept': 'application/json',
               }
