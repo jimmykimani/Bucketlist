044df8dcb19d824e20adaf4726689503df0fd35e
jimmykimani <james.kimani@andela.com>
2017-06-08 11:24:21 +0300

    (Fix) Clean up code

--- app/authentication.py
+++ app/authentication.py
@@ -1,5 +1,5 @@
 from flask import Flask, Blueprint, make_response, jsonify
-from flask_restful import reqparse, Resource, Api, inputs		 
+from flask_restful import reqparse, Resource, Api, inputs
 from app import db
 from app.models import User
 
