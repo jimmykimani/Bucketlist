f93695ee94536fe14784ed2a65f52394f378a275
jimmykimani <james.kimani@andela.com>
2017-06-08 10:48:10 +0300

    (FIx) Env

--- app/models.py
+++ app/models.py
@@ -32,7 +32,7 @@ class User(db.Model):
         To verify the auth_token, we use the same
         SECRET KEY used to encode a token
         """
-        s = Serializer(os.getenv('SECRET') or 'hard to guess string', expires_in=expiration)
+        s = Serializer(os.getenv('SECRET'), expires_in=expiration)
         # If the auth_token is valid, we get the
         # user id from the sub index of the payload.
         return s.dumps({'id': self.id})
